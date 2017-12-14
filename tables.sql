1a1870347c5809ad2845728e73ba50a97694b33b
razafaraz <raza.faraz@ug.bilkent.edu.tr>
2017-12-15 01:57:00 +0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -6,3 +6,12 @@ create table channel(
 create table private_channel(
     id int(10) unsigned primary key, 
     foreign key(id) references channel(id));
+    
+CREATE TABLE `database_project`.`bans` ( `bannedUserID` INT(10) NOT NULL AUTO_INCREMENT , `userID` INT(10) NOT NULL , PRIMARY KEY (`bannedUserID`)) ENGINE = InnoDB; 
+
+
+CREATE TABLE `database_project`.`remove_channel` ( `channelID` INT(10) NOT NULL AUTO_INCREMENT , `userID` INT(10) NOT NULL , PRIMARY KEY (`channelID`)) ENGINE = InnoDB; 
+
+CREATE TABLE `database_project`.`remove_post` ( `postID` INT(10) NOT NULL , `userID` INT(10) NOT NULL , PRIMARY KEY (`postID`)) ENGINE = InnoDB; 
+
+CREATE TABLE `database_project`.`remove_comment` ( `commentID` INT(10) NOT NULL AUTO_INCREMENT , `userID` INT(10) NOT NULL , PRIMARY KEY (`commentID`)) ENGINE = InnoDB;  
