f6cf77721a606909229e374390d83bfdaaf57fbe
razafaraz <raza.faraz@ug.bilkent.edu.tr>
2017-12-16 03:10:24 +0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -7,11 +7,128 @@ create table private_channel(
     id int(10) unsigned primary key, 
     foreign key(id) references channel(id));
     
-CREATE TABLE `database_project`.`bans` ( `bannedUserID` INT(10) NOT NULL AUTO_INCREMENT , `userID` INT(10) NOT NULL , PRIMARY KEY (`bannedUserID`)) ENGINE = InnoDB; 
 
 
-CREATE TABLE `database_project`.`remove_channel` ( `channelID` INT(10) NOT NULL AUTO_INCREMENT , `userID` INT(10) NOT NULL , PRIMARY KEY (`channelID`)) ENGINE = InnoDB; 
+//bann
 
-CREATE TABLE `database_project`.`remove_post` ( `postID` INT(10) NOT NULL , `userID` INT(10) NOT NULL , PRIMARY KEY (`postID`)) ENGINE = InnoDB; 
+CREATE TABLE `bans` (
+ `bannedUserID` int(10) NOT NULL AUTO_INCREMENT,
+ `userID` int(10) NOT NULL,
+ PRIMARY KEY (`bannedUserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-CREATE TABLE `database_project`.`remove_comment` ( `commentID` INT(10) NOT NULL AUTO_INCREMENT , `userID` INT(10) NOT NULL , PRIMARY KEY (`commentID`)) ENGINE = InnoDB;  
+
+//comments
+
+CREATE TABLE `comment` (
+ `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `body` varchar(8000) NOT NULL,
+ `since` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ `rating` int(11) NOT NULL DEFAULT '0',
+ `helpful_flag` int(10) NOT NULL DEFAULT '0',
+ `time_posted` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+ PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//removeChannel
+
+CREATE TABLE `remove_channel` (
+ `channelID` int(10) unsigned NOT NULL,
+ `userID` int(10) NOT NULL,
+ PRIMARY KEY (`channelID`),
+ CONSTRAINT `remove_channel_ibfk_1` FOREIGN KEY (`channelID`) REFERENCES `channel` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//remove post
+
+CREATE TABLE `remove_post` (
+ `postID` int(10) unsigned NOT NULL,
+ `userID` int(10) NOT NULL,
+ PRIMARY KEY (`postID`),
+ CONSTRAINT `remove_post_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `post` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+
+//remove Channel
+CREATE TABLE `remove_comment` (
+ `commentID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `userID` int(10) NOT NULL,
+ PRIMARY KEY (`commentID`),
+ CONSTRAINT `remove_comment_ibfk_1` FOREIGN KEY (`commentID`) REFERENCES `comment` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//remove commnent
+
+CREATE TABLE `remove_comment` (
+ `commentID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `userID` int(10) NOT NULL,
+ PRIMARY KEY (`commentID`),
+ CONSTRAINT `remove_comment_ibfk_1` FOREIGN KEY (`commentID`) REFERENCES `comment` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+
+//post
+CREATE TABLE `post` (
+ `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+ `title` varchar(800) NOT NULL,
+ `body` varchar(8000) NOT NULL,
+ `rating` int(11) DEFAULT '0',
+ `time_posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//has post
+CREATE TABLE `has_post` (
+ `postID` int(10) unsigned NOT NULL,
+ `channelID` int(11) unsigned NOT NULL,
+ PRIMARY KEY (`postID`),
+ KEY `channelID` (`channelID`),
+ CONSTRAINT `has_post_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `post` (`ID`),
+ CONSTRAINT `has_post_ibfk_2` FOREIGN KEY (`channelID`) REFERENCES `channel` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//post posts
+CREATE TABLE `posts_post` (
+ `postID` int(10) unsigned NOT NULL,
+ `userID` int(10) unsigned NOT NULL,
+ PRIMARY KEY (`postID`),
+ CONSTRAINT `posts_post_ibfk_1` FOREIGN KEY (`postID`) REFERENCES `post` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//replies
+CREATE TABLE `replies` (
+ `childCommentID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `parentCommentID` int(10) unsigned NOT NULL,
+ PRIMARY KEY (`childCommentID`),
+ KEY `parentCommentID` (`parentCommentID`),
+ CONSTRAINT `replies_ibfk_1` FOREIGN KEY (`childCommentID`) REFERENCES `comment` (`ID`),
+ CONSTRAINT `replies_ibfk_2` FOREIGN KEY (`parentCommentID`) REFERENCES `comment` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//has_comment
+CREATE TABLE `has_comment` (
+ `commentID` int(10) unsigned NOT NULL,
+ `postID` int(10) unsigned NOT NULL,
+ PRIMARY KEY (`commentID`),
+ KEY `postID` (`postID`),
+ CONSTRAINT `has_comment_ibfk_1` FOREIGN KEY (`commentID`) REFERENCES `comment` (`ID`),
+ CONSTRAINT `has_comment_ibfk_2` FOREIGN KEY (`postID`) REFERENCES `post` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+//postComments
+CREATE TABLE `posts_comment` (
+ `commentID` int(10) unsigned NOT NULL,
+ `userID` int(10) NOT NULL,
+ PRIMARY KEY (`commentID`),
+ CONSTRAINT `posts_comment_ibfk_1` FOREIGN KEY (`commentID`) REFERENCES `comment` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
