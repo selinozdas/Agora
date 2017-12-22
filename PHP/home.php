07e89776977b3f6b8a14b7d0c43fcd28b06bb69a
Raza <raza.faraz@ug.bilkent.edu.tr>
2017-12-22 06:56:08 +0300

    Added a search bar  that can search for post based on indexing and then deltes that index

--- PHP/home.php
+++ PHP/home.php
@@ -1,5 +1,37 @@
 <?PHP
    session_start();
+   include ("connection.php");
+
+    $output='';
+    if(isset($_POST['search'])){
+        $searchkey= $_POST['search'];
+        //$searchkey=preg_replace("#[^0-9a-z]#i", "", $searchkey);
+
+        //create index based on the post title
+        $sqlIndex= "CREATE INDEX index_post USING BTREE ON post(title)";
+        mysqli_query($con, $sqlIndex);
+
+        $sqlSearch = "SELECT * FROM post WHERE title LIKE '%$searchkey%'";
+        $query = mysqli_query($con,$sqlSearch) or die("Could not search!");
+        $count = mysqli_num_rows($query);
+
+        if($count == 0){
+            $output="There was no search result!";
+        }
+        else{
+            while($row=mysqli_fetch_array($query)){
+                $fname=$row['title'];
+
+                $output .='<div>'.$fname.'</div>';
+                echo "$output";
+
+            }
+        }
+        
+        //we drop the index now
+        $sqlDrop= "DROP INDEX index_post ON post";
+        mysqli_query($con, $sqlDrop);
+    }
 ?>
 
 
@@ -17,5 +49,11 @@
    <form id="logout_form" action="logout.php" method="post">
 	     <input type="Submit" value="Log Out" name="logoutbtn">
    </form>
+
+   <form id="search_form" action="home.php" method="post">
+	     <input type="text" name="search" placeholder="Search for post">
+         <input type= "submit" value= ">>"/>
+   </form>
+
 </body>
 </html>
