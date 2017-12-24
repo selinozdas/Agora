db4430d505f7c1997e4b14f055c56409a810c7b4
shompot <cholpon94@gmail.com>
2017-12-24 23:15:36 +0300

    home php

--- PHP/home.php
+++ PHP/home.php
@@ -1,7 +1,27 @@
 <?PHP
+   include ('connection.php');
    session_start();
-   include ("connection.php");
+   $username = $_SESSION['username'];
 
+   $query = 'SELECT ID, rating, title, time_posted
+   FROM
+       (SELECT channelID FROM subscribes WHERE userID = \''.$_SESSION['ID'].'\') AS T1
+       JOIN
+       (SELECT ID, rating, title, time_posted, channelID FROM post) AS T2
+       ON T1.channelID = T2.channelID
+   ORDER by rating DESC;';
+
+   $result = mysqli_query($con, $query);
+
+   $outRow ='<div class=\'postContainer\'>';
+   while($row = mysqli_fetch_array($result)) {
+      if($row) {
+
+         $outRow.='<a class=\'post\' href=#>'.$row['title'].'</a>';
+         $outRow.='<span class=\'timeData\'> Time: '.$row['time_posted'].'</span><br>';
+      }
+   }
+   $outRow.='</div>';
 ?>
 
 
@@ -12,50 +32,58 @@
 </head>
 <body>
    <?PHP if (isset($_SESSION['username'])){?>
-   <h3>Hello, <?PHP echo $_SESSION['username'];?> </h3>
-   <?PHP } else {?>
-   <h3>Home!</h3>
-   <?PHP }?>
+   <h3>Hello, <?PHP echo $username;?> </h3>
+   <?PHP } else {
+      header('Location: login.php');
+   }?>
    <form id="logout_form" action="logout.php" method="post">
 	     <input type="Submit" value="Log Out" name="logoutbtn">
    </form>
+   <div class='feed'>
+      <?PHP if (!empty($result)){
+         echo $outRow;
+      }?>
 
-   <form id="search_form" action="home.php" method="post">
-	     <input type="text" name="search" placeholder="Search for post">
-         <input type= "submit" value= ">>"/>
-   </form>
-
+   </div>
 </body>
 </html>
+<style>
+body{
+   background-color: lightgrey;
+}
+   .actionName {
+      font-size: 20px;
+      color: rgba(200,50,90,1);
+      margin-left: 30px;
+      width: 500px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+   }
+   .post {
+      margin-left: 50px;
+      font-size: 17px;
+      color: rgba(200,40,70,0.7);
+   }
+   .channelName {
+      font-size: 20px;
+      color: rgba(200,50,90,1);
+   }
+   .comment {
+      margin-left: 50px;
+      font-size: 17px;
+      color: rgba(200,40,70,0.7);
+   }
+   a:hover{
+      color: black;
+   }
 
-<?php
-    $output='';
-    if(isset($_POST['search'])){
-        $searchkey= $_POST['search'];
-        //$searchkey=preg_replace("#[^0-9a-z]#i", "", $searchkey);
-
-        //create index based on the post title
-        $sqlIndex= "CREATE INDEX index_post USING BTREE ON post(title)";
-        mysqli_query($con, $sqlIndex);
-
-        $sqlSearch = "SELECT * FROM post WHERE title LIKE '%$searchkey%'";
-        $query = mysqli_query($con,$sqlSearch) or die("Could not search!");
-        $count = mysqli_num_rows($query);
-
-        if($count == 0){
-            $output="There was no search result!";
-        }
-        else{
-            while($row=mysqli_fetch_array($query)){
-                $fname=$row['title'];
-                $output .='<div>'.$fname.'</div>';
-                echo "$output";
-
-            }
-        }
-        
-        //we drop the index now
-        $sqlDrop= "DROP INDEX index_post ON post";
-        mysqli_query($con, $sqlDrop);
-    }
-?>
+   span {
+      position: absolute;
+      margin-left: 450px;
+   }
+   .postContainer {
+      //border-style: solid;
+      //border-color: darkblue;
+      //border-width: 2px;
+   }
+</style>
