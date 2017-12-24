6bd554a1031d65696b851e618658c88caf12e9b9
Raza <raza.faraz@ug.bilkent.edu.tr>
2017-12-25 00:13:07 +0300

    Added a welcome page in php

---
+++ PHP/welcome.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<!-- your webpage info goes here -->
+	
+	<meta name="author" content="Group 8" />
+	<meta name="description" content="" />
+
+<!-- you should always add your stylesheet (css) in the head tag so that it starts loading before the page html is being displayed -->	
+	<link rel="stylesheet" href="../CSS/style.css" type="text/css" />
+	
+</head>
+<body>
+
+<!-- webpage content goes here in the body -->
+
+	<div id="page">
+		<div id="nav">
+			<ul>
+				<li><a href="home.php">home</a></li>
+				&nbsp; 
+				<li><a href="channels.php">channels</a></li>
+				&nbsp;  
+				<li><a href="login.php">login</a></li>
+				&nbsp;
+				<li><a href="search.php">search</a></li>
+				&nbsp; 
+			</ul>	
+		</div>
+		<div id="logo">
+			<h1><a href="index.php" id="logoLink">Agora.</a></h1>
+		</div>
+		<div id="footer">
+		    <ul>
+				<p><a href="about.php">about</a>
+				&nbsp; &nbsp; 
+				&nbsp; &nbsp; 
+				<a href="help.php">help</a>
+				&nbsp; &nbsp;
+				&nbsp; &nbsp; 
+				<a href="social.php">social</a>
+				&nbsp; &nbsp; 
+				&nbsp; &nbsp;
+				<a href="faq.php">faq</a></p>
+				&nbsp; &nbsp; 
+				&nbsp; &nbsp;
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
